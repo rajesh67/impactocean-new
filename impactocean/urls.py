5a1526ef01673d1fd8453c24ecdc13296213f653
rajesh67 <rajeshmeena.iitkgp@gmail.com>
2018-10-28 18:04:20 +0530

    second commit

--- impactocean/urls.py
+++ impactocean/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+
 from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from webapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^$', views.HomeView.as_view(), name="home"),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
